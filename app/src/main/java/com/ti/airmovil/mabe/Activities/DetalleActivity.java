c01d5b00544a2d929d8fa35a4e6b3fa6847c5928
jorgemanon <jorge.manon@airmovil.com>
2017-09-27 12:38:32 -0500

    Commit Jorge Detalle Producto

--- app/src/main/java/com/ti/airmovil/mabe/Activities/DetalleActivity.java
+++ app/src/main/java/com/ti/airmovil/mabe/Activities/DetalleActivity.java
@@ -2,44 +2,73 @@ package com.ti.airmovil.mabe.Activities;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Display;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.android.volley.Response;
 import com.ti.airmovil.mabe.R;
 import com.ti.airmovil.mabe.utilerias.AnimacionJAVA;
 
+import static com.ti.airmovil.mabe.R.id.imageView;
+
 public class DetalleActivity extends AppCompatActivity {
-    private static final String TAG = MainActivity.class.getSimpleName();
-    private Response.Listener listener;
-    private Response.ErrorListener listenerError;
 
     private ImageView ivFoto;
+    boolean banderaFoto = false;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_detalle);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
         ivFoto = (ImageView) findViewById(R.id.ivFoto);
         ivFoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                AnimacionJAVA.bigScaleAnimacion(ivFoto,1500);
+                Display display = getWindowManager().getDefaultDisplay();
+                int ancho = display.getWidth();
+                int  alto= display.getHeight();
+                ViewGroup.LayoutParams params = ivFoto.getLayoutParams();
+                if(banderaFoto){
+                    params.height = 600;
+                    ivFoto.setLayoutParams(params);
+                    banderaFoto = false;
+                }else{
+                    params.height = alto-15;
+                    ivFoto.setLayoutParams(params);
+                    banderaFoto = true;
+                }
             }
         });
-
     }
 
-    public void mensaje(){
+    public void salir(){
         AlertDialog.Builder dialog = new AlertDialog.Builder(this);
 
         dialog.setMessage("¿Salir?");
